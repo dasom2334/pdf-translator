19eba720ba533050941ab19f94f817446191c15f
cotton <cotton@tpcinternet.com>
2026-03-17 18:39:43 +0900

    Revert "Merge pull request #2 from dasom2334/feature/backend-init-boilerplate"
    This reverts commit 5ed196e9bf60a646050c843dfae74d87fa334743, reversing
    changes made to 28bc06f59e6e7f30ecaddbbb19dc243c30ab8f36.

--- src/app.module.ts
+++
@@ -1,8 +0,0 @@
-import { Module } from '@nestjs/common';
-import { PdfModule } from './pdf/pdf.module';
-import { TranslationModule } from './translation/translation.module';
-
-@Module({
-  imports: [PdfModule, TranslationModule],
-})
-export class AppModule {}
--- src/common/exceptions/translation.exception.ts
+++
@@ -1,7 +0,0 @@
-import { HttpException, HttpStatus } from '@nestjs/common';
-
-export class TranslationException extends HttpException {
-  constructor(message: string, status: HttpStatus = HttpStatus.INTERNAL_SERVER_ERROR) {
-    super(message, status);
-  }
-}
